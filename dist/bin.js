1f1b97f6eb9c8853575a7143b955f2c007cf59ca
Dotan Nahum <jondotan@gmail.com>
2018-04-19 23:06:07 +0300

    build

--- dist/bin.js
+++ dist/bin.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
-}
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const chalk_1 = __importDefault(require("chalk"));
 const execa_1 = __importDefault(require("execa"));
